5b7d66b0023eaa8f458268380aaf177b0d905be5
yuechen1 <yuechen1@live.com>
2017-01-26 11:06:57 -0800

    change to profs code, it doesn't run, something is missing?

--- Server.c
+++ Server.c
@@ -1,31 +1,53 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 
-int main()
+void error(const char *msg)
 {
-    int portnumber = 5000;
-    char buffer[1024];
-    int listen_fd, comm_fd;
-    struct sokaddr_in servaddr, cliaddr;
+   perror(msg);
+   exit(1);
+}
 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(portnumber);
-
-    bzero(&servaddr, sizeof(servaddr *) &servaddr, sizeof(servaddr));
-
-    listen(listen_fd, 5);
-
-    comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-
-    while(1)
-    {
-        bzero(buffer, 1024);
-        read(comm_fd, buffer, 1024);
-        write(comm_fd, buffer, strlen(buffer)+1)
-    }
-
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+       error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0) 
+             error("ERROR on binding");
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, 
+                (struct sockaddr *) &cli_addr, 
+                &clilen);
+    if (newsockfd < 0) 
+         error("ERROR on accept");
+    bzero(buffer,256);
+    n = read(newsockfd,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
 }

